2ab571d01a5010f7588a33b5482c7e2186fa522f
Marko Milanovic <markomilanovi02@gmail.com>
2022-07-14 15:22:51 +0200

    commit1

---
+++ 13./shirt-company/target/debug/deps/shirt_company-bae69cd5f380ea66.d
@@ -0,0 +1,5 @@
+/home/marko/Desktop/rustic/13./shirt-company/target/debug/deps/shirt_company-bae69cd5f380ea66: src/main.rs
+
+/home/marko/Desktop/rustic/13./shirt-company/target/debug/deps/shirt_company-bae69cd5f380ea66.d: src/main.rs
+
+src/main.rs:
