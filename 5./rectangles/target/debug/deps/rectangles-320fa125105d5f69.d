cae7c846a047fd02f923fa647312d2805d0ca402
Marko Milanovic <markomilanovi02@gmail.com>
2022-07-14 15:20:00 +0200

    commit1

---
+++ 5./rectangles/target/debug/deps/rectangles-320fa125105d5f69.d
@@ -0,0 +1,5 @@
+/home/marko/Desktop/rustic/5./rectangles/target/debug/deps/rectangles-320fa125105d5f69: src/main.rs
+
+/home/marko/Desktop/rustic/5./rectangles/target/debug/deps/rectangles-320fa125105d5f69.d: src/main.rs
+
+src/main.rs:
---
+++ 5./rectangles/target/debug/rectangles.d
@@ -0,0 +1,1 @@
+/home/marko/Desktop/rustic/5./rectangles/target/debug/rectangles: /home/marko/Desktop/rustic/5./rectangles/src/main.rs
