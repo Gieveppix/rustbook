687b41e76c843afe1da6aae3e5b43462539c2695
Marko Milanović <mmilanovic@XX240.local>
2022-07-18 13:35:43 +0200

    Threads

---
+++ 16./threads/target/debug/deps/threads-5ea4b7f4c57c45ae.d
@@ -0,0 +1,5 @@
+/Users/mmilanovic/Documents/RustTest/16./threads/target/debug/deps/threads-5ea4b7f4c57c45ae.rmeta: src/main.rs
+
+/Users/mmilanovic/Documents/RustTest/16./threads/target/debug/deps/threads-5ea4b7f4c57c45ae.d: src/main.rs
+
+src/main.rs:
---
+++ 16./threads/target/debug/deps/threads-5f9f322e98d50a60.d
@@ -0,0 +1,5 @@
+/Users/mmilanovic/Documents/RustTest/16./threads/target/debug/deps/threads-5f9f322e98d50a60: src/main.rs
+
+/Users/mmilanovic/Documents/RustTest/16./threads/target/debug/deps/threads-5f9f322e98d50a60.d: src/main.rs
+
+src/main.rs:
