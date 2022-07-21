3c429501b091a138564e933fd76d82a558b9f6d3
Marko Milanović <mmilanovic@XX240.local>
2022-07-21 14:06:00 +0200

    19 and start of 20

---
+++ 20./projecthello/target/debug/deps/projecthello-733ede7e9d5d2e09.d
@@ -0,0 +1,5 @@
+/Users/mmilanovic/Documents/RustTest/20./projecthello/target/debug/deps/projecthello-733ede7e9d5d2e09.rmeta: src/main.rs
+
+/Users/mmilanovic/Documents/RustTest/20./projecthello/target/debug/deps/projecthello-733ede7e9d5d2e09.d: src/main.rs
+
+src/main.rs:
---
+++ 20./projecthello/target/debug/deps/projecthello-d520fe8f21f2014d.d
@@ -0,0 +1,5 @@
+/Users/mmilanovic/Documents/RustTest/20./projecthello/target/debug/deps/projecthello-d520fe8f21f2014d.rmeta: src/main.rs
+
+/Users/mmilanovic/Documents/RustTest/20./projecthello/target/debug/deps/projecthello-d520fe8f21f2014d.d: src/main.rs
+
+src/main.rs:
